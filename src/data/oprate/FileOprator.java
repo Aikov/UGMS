da152a55242848cd8981a6baf7ea24d95c05b225
King Aikov <dzhlong@outlook.com>
2019-10-11 18:51:25 +0800

    可tm把读取逻辑写完了，为什么不让用数据库啊啊啊啊啊

--- src/data/oprate/FileOprator.java
+++ src/data/oprate/FileOprator.java
@@ -5,32 +5,44 @@ import data.save.Student;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class FileOprator {
     private String name;
     private String PathIn = "\\Github\\UGMS\\FileIn\\";       //TODO：交作业的时候把这里的Github删掉
     private String PathOut = "\\Github\\UGMS\\FileOut\\";
+    private String PathOutput = "\\Github\\UGMS\\FileOut\\Student\\";
     private String Path;
+    public Student[] students;
 
-    public FileOprator(String Filename, int mode) {
-        // Mode 0 : read the file processed by program
-        // Mode 1 : read the given file
-        // Mode 2 : Output file
-        this.name = Filename;
-        if (1 == mode) this.Path = PathIn + this.name;
-        else this.Path = PathOut + this.name;
+    public FileOprator(Student[] Info) {
+        this.students = Info;
     }
 
-    public void read(Student[] students) {
+    public void read(String FileName) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(this.Path));
+            BufferedReader reader = new BufferedReader(new FileReader(FileName));
             String FirstLine = reader.readLine();
             String[] BasicInfo = FirstLine.split("\\s");    //我为什么总是想着正则不放，WHY？
-            reader.readLine();
-            for (Student stu : students) {
-                stu.setCourseBasicInfo(BasicInfo[0], Integer.parseInt(BasicInfo[1]));
+            int Num = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < Num; i++) {
+                boolean isExist = true;
+                String line = reader.readLine();
+                String[] info = line.split(",");
+                for (Student stu : students) {
+                    if (stu.StudentID.equals(info[2])) {
+                        stu.setScore(BasicInfo[0], Double.parseDouble(info[3]));
+                        isExist = true;
+                        break;
+                    } else isExist = false;
+                }
+                if (!isExist) {
+                    int posi;
+                    for (posi = 0; students[posi].StudentID != null; posi++) ;
+                    students[posi].Firstname = info[0];
+                    students[posi].Lastname = info[1];
+                    students[posi].StudentID = info[2];
+                    students[posi].setScore(BasicInfo[0], Double.parseDouble(info[3]));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/data/save/Student.java
+++ src/data/save/Student.java
@@ -6,16 +6,39 @@ public class Student {
     public String Firstname;
     public String Lastname;
     public String StudentID;
-    public float score;
+    public double GPA;
     Course[] course = new Course[100];
 
-    public void setCourseBasicInfo(String CourseName, int Credit) {
+    public boolean setCourseBasicInfo(String CourseName, int Credit) {
         for (Course cour : course) {
-            if (cour.CourseName.equals(CourseName)) return;  // Skip init if there is the same
+            if (cour.CourseName.equals(CourseName)) return false;  // Skip init if there is the same
         }
         int i;
         for (i = 0; course[i].CourseName != null; i++) continue;
         course[i+1].CourseName = CourseName;
         course[i+1].CourseCredit = Credit;
+        return true;
+    }
+    public void setScore(String CourseName, double Score){
+        for(Course cour :course){
+            if(cour.CourseName.equals(CourseName)) cour.CourseScore = Score;
+            try{
+                cour.CalculatePointsAndGrade();
+            }catch(RuntimeException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public void CaluateGPA(){
+        double Ctotal = 0;
+        double Ptotal = 0;
+        for(Course cour:course){
+            if (cour.CourseName != null) {
+                Ctotal += cour.CourseCredit * cour.CourseScore;
+                Ptotal += cour.CourseCredit;
+            }
+            else break;
+        }
+        this.GPA = Ctotal/Ptotal ;
     }
 }
