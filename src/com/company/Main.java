debaa7f239e46c10c0a54e7861d2ebaba62945ed
King Aikov <dzhlong@outlook.com>
2019-10-11 17:40:49 +0800

    我有种这个玩意早晚得重构的预感

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
-import data.oprate.FileOprator;
-
 public class Main {
     public static void main(String[] args) {
-        FileOprator fileOprator = new FileOprator("Bye.txt",1);
-        fileOprator.read();
+
     }
 }
