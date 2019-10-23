644947ffa7daaaca0b0478524f98468be6e51741
King Aikov <dzhlong@outlook.com>
2019-10-23 22:43:10 +0800

    开始写主界面咯
    其实我还是不大想写这玩意的
    
    就这样吧

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,33 +5,45 @@ import data.oprate.Tool;
 import data.save.Student;
 
 import java.awt.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Student[] students = new Student[10000];
+        FileOperator fileOperator = new FileOperator(students);
+        Tool t = new Tool();
         for (int i = 0; i < students.length; i++) {
             students[i] = new Student();
         }
-        FileOperator fileOperator = new FileOperator(students);
-        fileOperator.read("CN105.txt");
-        System.out.println("Finished");
-        fileOperator.read("LP002.txt");
-        System.out.println("Finished");
-        fileOperator.read("LP104.txt");
-        System.out.println("Finished");
-        fileOperator.read("MA109.txt");
-        for(Student stu:students){
-            if(stu.StudentID.equals(""))
+        do {
+            System.out.print("Please enter the database filename:");
+            String course = input.nextLine();
+            System.out.println("Do you have another score report?\n Press \"Y\" for Yes and \"N\" for No");
+            fileOperator.read(course);
+        } while (input.nextLine().equals("Y"));
+        Student[] selected = t.select_score(students, "CN105");
+        t.Sort_Course(selected, 1);
+        /*for (Student stu : selected) {
+            if (stu.StudentID.equals(""))
                 break;
-            else System.out.println(stu.Told());
-        }
-        Tool t = new Tool();
-        Student[] selected = t.select_score(students,"CN105");
-        t.Sort_Course(selected,1);
-        for(Student stu:selected){
-            if(stu.StudentID.equals(""))
-                break;
-            else System.out.println(stu.StudentID + " " +stu.selected.CourseScore);
+            else System.out.println(stu.StudentID + " " + stu.selected.CourseScore);
+        }*/
+        print_course(selected);
+    }
+
+    private static void print_course(Student[] info) {
+        System.out.println("Course Code: " + info[0].selected.CourseName);
+        System.out.println("Credit: " + info[0].selected.CourseCredit);
+        System.out.println("Number of Students: " + info.length);
+        System.out.println("Name               ID                       Score      Grade ");
+        System.out.println("--------------     ------------------       ------     ------");
+        for (Student stu : info) {
+            String name = stu.FirstName + "," + stu.Surname;
+            System.out.printf("%-14s    ", name);
+            System.out.printf("%19s       ", stu.StudentID);
+            System.out.printf("%6s     ", stu.selected.CourseScore);
+            System.out.printf("%-6s\n", stu.selected.CourseGrade);
         }
     }
 }
